613343182d05cd5d946cdf399c0737c806209a7c
Kuba Klin <poczta@kubaklin.pl>
2025-03-05 15:39:01 +0100

    feat: move buttons to index.html with descriptions

--- src/OrganismPopup.js
+++ src/OrganismPopup.js
@@ -4,19 +4,22 @@ export class OrganismPopup {
     this.popup = document.getElementById('organism-popup');
     this.buttonsContainer = document.getElementById('organism-buttons');
     this.cancelButton = document.getElementById('cancel-popup');
-    this.organisms = [
-      'Wolf',
-      'Sheep',
-      'Fox',
-      'Antelope',
-      'Turtle',
-      'Grass',
-      'Guarana',
-      'PoisonBerry',
-      'SowThistle',
-    ];
 
+    this.initializeButtonsHandler();
     this.initializeCancelHandler();
+  }
+
+  initializeButtonsHandler() {
+    const buttons = this.buttonsContainer.querySelectorAll('.add-animal-button');
+    buttons.forEach((button) => {
+      const organism = button.getAttribute('data-organism');
+      button.addEventListener('click', () => {
+        const positionY = button.getAttribute('data-position-y');
+        const positionX = button.getAttribute('data-position-x');
+        this.board.createOrganism(organism, positionY, positionX);
+        this.closePopup();
+      });
+    });
   }
 
   initializeCancelHandler() {
@@ -26,30 +29,15 @@ export class OrganismPopup {
   }
 
   showPopup(positionY, positionX) {
-    // Clear previous buttons
-    this.clearButtons();
-
-    // Add buttons for each organism type
-    this.organisms.forEach((organism) => {
-      const button = document.createElement('button');
-      button.textContent = organism;
-      button.addEventListener('click', () => {
-        this.board.createOrganism(organism, positionY, positionX);
-        this.closePopup();
-      });
-      this.buttonsContainer.appendChild(button);
-    });
-
     this.popup.classList.add('active');
+    const buttons = this.buttonsContainer.querySelectorAll('.add-animal-button');
+    buttons.forEach((button) => {
+      button.setAttribute('data-position-y', positionY);
+      button.setAttribute('data-position-x', positionX);
+    });
   }
 
   closePopup() {
     this.popup.classList.remove('active');
-  }
-
-  clearButtons() {
-    while (this.buttonsContainer.firstChild) {
-      this.buttonsContainer.removeChild(this.buttonsContainer.firstChild);
-    }
   }
 }
